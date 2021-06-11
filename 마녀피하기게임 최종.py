d59ad9a6e027e1040965f49cd7c01a8c3bc82fe2
seohyun0624 <84948655+seohyun0624@users.noreply.github.com>
2021-06-11 11:25:20 +0900

    Add files via upload

---
+++ 마녀피하기게임 최종.py
@@ -0,0 +1,84 @@
+import pygame  
+import random
+import os
+
+pygame.init()  
+
+
+
+BLACK = (0, 0, 0)
+size = [600, 800]
+screen = pygame.display.set_mode(size)
+
+done = False
+clock = pygame.time.Clock()
+
+def runGame():
+    witch_image = pygame.image.load('witch.png')
+    witch_image = pygame.transform.scale(witch_image, (80, 80))
+    witchs = []
+
+    for i in range(5):
+        rect = pygame.Rect(witch_image.get_rect())
+        rect.left = random.randint(0, size[0])
+        rect.top = -100
+        dy = random.randint(20, 25)
+        witchs.append({'rect': rect, 'dy': dy})
+
+    person_image = pygame.image.load('person.png')
+    person_image = pygame.transform.scale(person_image, (80, 80))
+    person = pygame.Rect(person_image.get_rect())
+    person.left = size[0] // 2 - person.width // 2
+    person.top = size[1] - person.height
+    person_dx = 0
+    person_dy = 0
+
+    global done
+    while not done:
+        clock.tick(30)
+        screen.fill(BLACK)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                break
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    person_dx = -10
+                elif event.key == pygame.K_RIGHT:
+                    person_dx = 10
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    person_dx = 0
+                elif event.key == pygame.K_RIGHT:
+                    person_dx = 0
+
+        for witch in witchs:
+            witch['rect'].top += witch['dy']
+            if witch['rect'].top > size[1]:
+                witchs.remove(witch)
+                rect = pygame.Rect(witch_image.get_rect())
+                rect.left = random.randint(0, size[0])
+                rect.top = -100
+                dy = random.randint(3,9)
+                witchs.append({'rect': rect, 'dy': dy})
+
+        person.left = person.left + person_dx
+
+        if person.left < 0:
+            person.left = 0
+        elif person.left > size[0] - person.width:
+            person.left = size[0] - person.width
+
+        screen.blit(person_image, person)
+
+        for witch in witchs:
+            if witch['rect'].colliderect(person):
+                done = True
+            screen.blit(witch_image, witch['rect'])
+
+        pygame.display.update()
+
+
+runGame()
+pygame.quit()
